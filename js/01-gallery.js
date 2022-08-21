2ef0ec6bb343dcd650ef593798b87ec44c8eaff8
ValeriyLitvin <valerkosssss@gmail.com>
2022-08-21 13:11:22 +0300

    21.08.2022

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryDiv = document.querySelector(".gallery");
+const createImaneFn = createImageCard({ galleryItems });
+
+galleryDiv.insertAdjacentHTML("beforeend", createImaneFn);
+
+galleryDiv.addEventListener("click", callOriginalPicture);
+
+function createImageCard({ galleryItems }) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join("");
+}
+
+function callOriginalPicture(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const refBigImg = evt.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${refBigImg}">
+`);
+  instance.show();
+  galleryDiv.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryDiv = document.querySelector(".gallery");
+const createImaneFn = createImageCard({ galleryItems });
+
+galleryDiv.insertAdjacentHTML("beforeend", createImaneFn);
+
+galleryDiv.addEventListener("click", callOriginalPicture);
+
+function createImageCard({ galleryItems }) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+             </a>`;
+    })
+    .join("");
+}
+function callOriginalPicture(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const lightbox = new SimpleLightbox(".gallery a", {
+    navText: ["👈", "👉"],
+    captionsData: "alt",
+    showCounter: false,
+    captionDelay: 250,
+  });
+}
